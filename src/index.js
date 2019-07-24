70c9534356c05f57195e3bf41db181231f4db94a
test <test@MacBook-Pro-2.local>
2019-07-24 15:06:07 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import '../src/css/general.css';
+import '../src/css/header.css';
+import '../src/css/catalog.css';
+import '../src/css/facilities.css';
+import '../src/css/footer.css';
+
+
+import '../src/app/script';
+import '../src/scss/main.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'jquery/dist/jquery.slim.min';
+import 'popper.js';
+import 'bootstrap/dist/js/bootstrap.min';
+
+let add = (a,b) => a+b
+console.log(add(2,6))
+
+
